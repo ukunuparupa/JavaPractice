17800df1b63ff9cb302908831f030e1b7fddf684
Motsuka <haguremeta.9806@gmail.com>
2023-04-26 00:33:28 +0900

    ファイル名修正

---
+++ exercise/New_employee_training/elementary/Practice2.java
@@ -0,0 +1,9 @@
+class Practice2{
+	public static void main(String args[]){
+		int a, b, c;
+		a = 10;
+		b = 20;
+		c = 30;
+		System.out.println(a + "，" + b + "，" + c + "の合計は" + a + b + c + "です");
+	}
+}
