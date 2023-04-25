172a012032c3462d5e7f63094c7cca08c90af5c0
ukunuparupa <63774331+ukunuparupa@users.noreply.github.com>
2023-04-26 00:34:43 +0900

    Delete Exercise2_1.java
    ファイル名修正につき削除

--- exercise/New_employee_training/elementary/Exercise2_1.java
+++
@@ -1,9 +0,0 @@
-class Exercise2_1{
-	public static void main(String args[]){
-		int a, b, c;
-		a = 10;
-		b = 20;
-		c = 30;
-		System.out.println(a + "，" + b + "，" + c + "の合計は" + a + b + c + "です");
-	}
-}
