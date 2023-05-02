7cb324659593bb1b14f782f4bed1c885f484e652
Motsuka <haguremeta.9806@gmail.com>
2023-05-03 01:45:37 +0900

    演習問題10

--- exercise/New_employee_training/elementary/Practice10.java
+++ exercise/New_employee_training/elementary/Practice10.java
@@ -2,11 +2,14 @@ class Practice10{
 	public static void main(String args[]){
 		int matrixSize = 9;
 		int times = 0;
-		for(int i; i < matrixSize; i++){
-			for(int j; j < matrixSize; j++){
-				times = j * j;
-				System.out.println(times + "");
+		String str = "";
+		for(int i = 1; i <= matrixSize; i++){
+			for(int j = 1; j <= matrixSize; j++){
+				times = i * j;
+				str = String.format("%3d", times);
+				System.out.print(str);
 			}
+			System.out.println();
 		}
 	}
 }
