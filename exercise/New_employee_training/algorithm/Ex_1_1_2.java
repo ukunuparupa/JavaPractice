d2c1e1a7bfcc4b19ac84cac38a3746ab5abf6b04
Motsuka <haguremeta.9806@gmail.com>
2023-05-03 13:49:44 +0900

    演習問題1.1-2を解いた．

---
+++ exercise/New_employee_training/algorithm/Ex_1_1_2.java
@@ -0,0 +1,12 @@
+class Ex_1_1_2{
+	public static void main(String args[]){
+		int[] data = {13, 24, 8, 67, 42, 9, 33, 92, 24, 57};
+		int target = 9;
+		int dataLength = data.length;
+		int index = 0;
+		boolean exists = false;
+
+		while(index < dataLength){
+		}
+	}
+}
