759d4069bbd421c9200c8aa6a4e170e9fcc60543
Motsuka <haguremeta.9806@gmail.com>
2023-05-14 01:26:54 +0900

    必要のないメソッドを削除

--- exercise/New_employee_training/algorithm/Ex3_4_2.java
+++ exercise/New_employee_training/algorithm/Ex3_4_2.java
@@ -32,32 +32,4 @@ class Ex3_4_2{
 			System.out.print(data[i] + ",");
 		}
 	}
-
-	//入力値のint配列を昇順ソートする
-	//Ex3_3_2の挿入ソートをそのまま実行
-	//
-	//入力値：昇順にソートしたいint配列
-	//出力値：なし
-	static void sort(int[] data){
-		int temp = 0;
-
-		for(int i = 1; i < data.length; i++){
-			temp = data[i];
-			for(int j = i - 1; 0 <= j; j--){
-				//tempが小さい時にはデータをスライドさせる．
-				//本の場所を移動させるイメージ
-				if(temp < data[j]){
-					data[j + 1] = data[j];
-
-					//先頭に来た時には，tempを代入してしまう．
-					if(j == 0){
-						data[j] = temp;
-					}
-				}else if(data[j] < temp){
-					data[j + 1] = temp;
-					break;
-				}
-			}
-		}
-	}
 }
