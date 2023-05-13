3d16b4513cc012a9ba39f67cf79676a13a285d86
Motsuka <haguremeta.9806@gmail.com>
2023-05-14 01:25:13 +0900

    shell soutを実装

--- exercise/New_employee_training/algorithm/Ex3_4_2.java
+++ exercise/New_employee_training/algorithm/Ex3_4_2.java
@@ -1,10 +1,35 @@
 class Ex3_4_2{
 	public static void main(String args[]){
 		int[] data = {13, 28, 10, 40, 42, 57, 27, 25, 66, 3, 18, 54, 7, 20, 33, 5, 12};
+		int index = 0;
+		int mod = 1;
+		int temp = 0;
 
-		sort(data);
+		while(0 < mod){
+			//挿入ソート
+			//modを法とした配列だけを並べ替える．
+			for(int i = 1; i < data.length; i++){
+				temp = data[i];
+				for(int j = i - mod; 0 <= j; j = j - mod){
+					if(temp < data[j]){
+						data[j + mod] = data[j];
+
+						//先頭に来た時には，tempを代入してしまう．
+						if(j == 0){
+							data[j] = temp;
+						}
+					}else if(data[j] < temp){
+						data[j + mod] = temp;
+						break;
+					}
+				}
+			}
+			mod = mod / 2;
+		}
+
+		//処理結果表示
 		for(int i = 0; i < data.length; i++){
-			System.out.print(data[i] + ", ");
+			System.out.print(data[i] + ",");
 		}
 	}
 
